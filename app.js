b703c60eb0aed9c3c3e86422a096e11da217c81f
ranjan7707 <ranjan7707@gmail.com>
2023-07-04 20:22:40 +0530

    Q7 backened

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const http = require('http');
 
 http.createServer((req,resp)=>{
     console.log(req.url,req.method,req.header);
-    resp.writeHead('Content-Type','text/html');
+    resp.setHeader('Content-Type','text/html');
     resp.write('<html>');
-    resp.write('<head>This is my First Project</head>');
-    resp.write('<body>This is the body of my Project</body>');
+    resp.write('<head><title>This is my First Project</title></head>');
+    resp.write('<body><h1>This is the body of my Project</h1></body>');
     resp.write('</html>');
     resp.end();
-}).listen(3100);
+}).listen(4000);
